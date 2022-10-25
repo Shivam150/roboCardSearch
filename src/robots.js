6754f383a61f2421b99bd5f28f82fb52db8ca042
Shivam <shivamsingh3754966@gmail.com>
2022-10-25 23:26:03 +0530

    first commit

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+
+{ id: 1,
+  name: 'Shivam Singh',
+  userName : 'shiv12',
+  email : 'shiv123@gmail.com'
+},
+{ id: 2,
+    name: 'Golu Singh',
+    userName : 'golu12',
+    email : 'golu123@gmail.com'
+},
+{ id: 3,
+    name: 'Ankit Singh',
+    userName : 'ank12',
+    email : 'ankit123@gmail.com'
+},
+{ id: 4,
+    name: 'Karan Singh',
+    userName : 'kar12',
+    email : 'karan123@gmail.com'
+},
+{ id: 5,
+    name: 'Gagan Yadav',
+    userName : 'gagan12',
+    email : 'gagan123@gmail.com'
+},
+{
+    id: 6,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+},
+{
+    id: 7,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+},
+{
+    id: 8,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+}
+
+
+
+
+];
