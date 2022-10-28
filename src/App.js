18fe42d3de296c9c5fc30ceba9c7d706795dd92a
Shivam <shivamsingh3754966@gmail.com>
2022-10-29 00:24:19 +0530

    Card bg changed

--- src/App.js
+++ src/App.js
@@ -22,6 +22,10 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots=>{
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
+        if(robots.length===0){
+            return <h1>Loading</h1>
+        }
+        else {
 
         return (
 
@@ -32,6 +36,7 @@ class App extends Component {
             </div>
 
         );
+        }
    }
 }
 
--- src/robots.js
+++ src/robots.js
@@ -1,44 +1,44 @@
 export const robots = [
 
-{ id: 1,
+{ id: 100,
   name: 'Shivam Singh',
   userName : 'shiv12',
   email : 'shiv123@gmail.com'
 },
-{ id: 2,
+{ id: 200,
     name: 'Golu Singh',
     userName : 'golu12',
     email : 'golu123@gmail.com'
 },
-{ id: 3,
+{ id: 300,
     name: 'Ankit Singh',
     userName : 'ank12',
     email : 'ankit123@gmail.com'
 },
-{ id: 4,
+{ id: 400,
     name: 'Karan Singh',
     userName : 'kar12',
     email : 'karan123@gmail.com'
 },
-{ id: 5,
+{ id: 500,
     name: 'Gagan Yadav',
     userName : 'gagan12',
     email : 'gagan123@gmail.com'
 },
 {
-    id: 6,
-    name: 'Leanne Graham',
-    username: 'Bret',
+    id: 600,
+    name: 'Subrat Katiyar',
+    username: 'subrat12',
     email: 'Sincere@april.biz'
 },
 {
-    id: 7,
+    id: 700,
     name: 'Ervin Howell',
     username: 'Antonette',
     email: 'Shanna@melissa.tv'
 },
 {
-    id: 8,
+    id: 800,
     name: 'Patricia Lebsack',
     username: 'Karianne',
     email: 'Julianne.OConner@kory.org'
